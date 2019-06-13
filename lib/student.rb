29467cf50a5b409e13863e4ba6785813393c4996
Melissa Williams <melissa@brandmeetscopy.com>
2019-06-13 05:06:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,12 +5,12 @@ class Student < User
   @knowledge = []
   end
 
-def learn(string)
-  @knowledge << string
-end
+  def learn(string)
+    @knowledge << string
+  end
 
-def knowledge
-  @knowledge
-end
+  def knowledge
+    @knowledge
+  end
 
 end
